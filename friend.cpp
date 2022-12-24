69222c654b806b7c61b6d039a68a505e78bc433d
Anuta187 <115971056+Anuta187@users.noreply.github.com>
2022-12-25 02:38:18 +1000

    Предметная область
    Дружественные функции и модули.

---
+++ friend.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include "modul.cpp"
+int main()
+{
+    Auto tesla("Tesla", 5000);
+    drive(tesla);
+    std::cout << tesla.getName() << " : " << tesla.getPrice() << std::endl;
+    setPrice(tesla, 8000);
+    std::cout << tesla.getName() << " : " << tesla.getPrice() << std::endl;
+
+    return 0;
+};
+void drive(Auto& a)
+{
+    std::cout << a.name << " is driven" << std::endl;
+}
+void setPrice(Auto& a, int price)
+{
+    if (price > 0)
+        a.price = price;
+}
---
+++ modul.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>  
+
+class Auto
+{
+    friend void drive(Auto&);
+    friend void setPrice(Auto&, int price);
+public:
+    Auto(std::string autoName, int autoPrice)
+    {
+        name = autoName;
+        price = autoPrice;
+    }
+    std::string getName() { return name; }
+    int getPrice() { return price; }
+
+private:
+    std::string name;   // название автомобиля
+    int price;  // цена автомобиля
+};
+
